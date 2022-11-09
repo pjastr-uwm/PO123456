b675caff03316f254f4c696b09e09e92bfe2d7a7
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2022-11-09 15:27:14 +0100

    cw5

--- cw5/zad1/liczby.py
+++ cw5/zad1/liczby.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from math import gcd
 
 
@@ -22,3 +23,49 @@ class Wymierna:
 
     def __repr__(self) -> str:
         return f"{self.p}/{self.q}"
+
+    def get_licznik(self) -> int:
+        return self.p
+
+    def get_mianownik(self) -> int:
+        return self.q
+
+    def __float__(self) -> float:
+        return self.p / self.q
+
+    def __add__(self, other: Wymierna) -> Wymierna:
+        return Wymierna(self.p * other.q + self.q * other.p, self.q * other.q)
+
+    def __sub__(self, other: Wymierna) -> Wymierna:
+        return Wymierna(self.p * other.q - self.q * other.p, self.q * other.q)
+
+    # def __eq__(self, other: Wymierna) -> bool:
+    #     return self.p == other.p and self.q == other.q
+
+    def __ne__(self, other: Wymierna) -> bool:
+        return self.p != other.p or self.q != other.q
+
+    def __lt__(self, other: Wymierna) -> bool:
+        return self.p * other.q < self.q * other.p
+
+    def __le__(self, other: Wymierna) -> bool:
+        return self.p * other.q <= self.q * other.p
+
+    def __gt__(self, other: Wymierna) -> bool:
+        return self.p * other.q > self.q * other.p
+
+    def __ge__(self, other: Wymierna) -> bool:
+        return self.p * other.q >= self.q * other.p
+
+    def __mul__(self, other:Wymierna) -> Wymierna:
+        return Wymierna(self.p*other.p, self.q*other.q)
+
+    def __truediv__(self, other:Wymierna) ->Wymierna:
+        if other.p == 0:
+            raise ValueError("Nie dziel przez zero!")
+        return Wymierna(self.p*other.q, self.q*other.p)
+
+    def __eq__(self, other:Wymierna)->bool:
+        return not(self<other) and not(other<self)
+
+
--- cw5/zad1/main.py
+++ cw5/zad1/main.py
@@ -2,8 +2,29 @@ from liczby import Wymierna
 
 
 def main():
-    w1: Wymierna = Wymierna(0, -25)
+    w1: Wymierna = Wymierna(4, 2)
     print(w1)
+    print(float(w1))
+    print(type(float(w1)))
+    w2: Wymierna = Wymierna(1, 2)
+    w3: Wymierna = Wymierna(1, -3)
+    print(w2 + w3)
+    print(w2 - w3)
+    w4: Wymierna = Wymierna(5, 10)
+    print(w2 == w4)
+    print(w2 != w4)
+    print(w2 < w4)
+    print(w2 <= w4)
+    print(w2 > w4)
+    print(w2 >= w4)
+    print(w2 < w3)
+    print(w2 <= w3)
+    print(w2 > w3)
+    print(w2 >= w3)
+    print(w2*w3)
+    print(w2 / w3)
+    zero: Wymierna = Wymierna()
+    # print(w2/zero)
 
 
 if __name__ == "__main__":
