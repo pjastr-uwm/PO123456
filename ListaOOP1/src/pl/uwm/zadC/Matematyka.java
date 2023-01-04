402cb8252651e018da9804414809820edb3b21ef
pjastr <24464854+pjastr@users.noreply.github.com>
2023-01-04 14:03:44 +0100

    mat

---
+++ ListaOOP1/src/pl/uwm/zadC/Matematyka.java
@@ -0,0 +1,13 @@
+package pl.uwm.zadC;
+
+public class Matematyka {
+    public static double getObwod(double promien)
+    {
+        return 2* pi* promien;
+    }
+    public static double getArea(double promien)
+    {
+        return pi*promien*promien;
+    }
+    static double pi=3.14;
+}
---
+++ ListaOOP1/src/pl/uwm/zadC/TestMatematyka.java
@@ -0,0 +1,10 @@
+package pl.uwm.zadC;
+
+public class TestMatematyka {
+
+    public static void main(String[] args)
+    {
+        System.out.println(Matematyka.getObwod(1));
+        System.out.println(Matematyka.getArea(1));
+    }
+}
